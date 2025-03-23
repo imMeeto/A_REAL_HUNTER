15abafa345621313db87016647823fd5a7c8af9a
Your Name <meeto.collab@gmail.com>
2025-03-23 10:28:22 +0300

    save

--- Source/YourGame/Public/DogSystem/DogCharacter.h
+++ Source/YourGame/Public/DogSystem/DogCharacter.h
@@ -1,47 +1,17 @@
 #pragma once
+#include "Include/DogSystem/DogTypes.h"
+#include "Include/DogSystem/HuntingTargetInterface.h"
 
-#include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "DogCharacter.generated.h"
-
-UENUM(BlueprintType)
-enum class EDogState : uint8
-{
-    Idle,
-    Following,
-    Playing,
-    Sitting,
-    Sleeping
-};
-
-UCLASS()
-class YOURGAME_API ADogCharacter : public ACharacter
+class YOURGAME_API ADogCharacter : public AActor
 {
     GENERATED_BODY()
 
 public:
-    ADogCharacter();
-
-    virtual void Tick(float DeltaTime) override;
-    virtual void BeginPlay() override;
-
-    UFUNCTION(BlueprintCallable, Category = "Dog System")
-    void InteractWithPlayer(class ACharacter* Player);
-
-    UFUNCTION(BlueprintCallable, Category = "Dog System")
-    void UpdateDogState(EDogState NewState);
+    static FText GetDogStateText(EDogState State);
 
 protected:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dog System")
-    float InteractionRadius;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dog System")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dog")
     EDogState CurrentState;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dog System")
-    float FollowDistance;
-
 private:
-    void UpdateBehavior(float DeltaTime);
-    bool IsPlayerInRange() const;
 };
